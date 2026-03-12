8e847cf06097c29b474b5d26786ad4f3e2316d4a
infinityabundance <255699974+infinityabundance@users.noreply.github.com>
2026-03-12 03:48:33 +0000

    Remove dsfb-srd build artifacts

--- crates/dsfb-srd/target/release/build/num-traits-997a0755fee0384f/build_script_build-997a0755fee0384f.d
+++
@@ -1,5 +0,0 @@
-/home/one/dsfb/crates/dsfb-srd/target/release/build/num-traits-997a0755fee0384f/build_script_build-997a0755fee0384f.d: /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/one/dsfb/crates/dsfb-srd/target/release/build/num-traits-997a0755fee0384f/build_script_build-997a0755fee0384f: /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs:
