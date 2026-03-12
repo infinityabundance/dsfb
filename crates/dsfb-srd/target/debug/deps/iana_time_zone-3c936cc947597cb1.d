3861d73089579cd2f7fc6af9ace745c5878edf34
infinityabundance <255699974+infinityabundance@users.noreply.github.com>
2026-03-12 03:48:33 +0000

    Remove dsfb-srd build artifacts

--- crates/dsfb-srd/target/debug/deps/iana_time_zone-3c936cc947597cb1.d
+++
@@ -1,9 +0,0 @@
-/home/one/dsfb/crates/dsfb-srd/target/debug/deps/iana_time_zone-3c936cc947597cb1.d: /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/lib.rs /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/ffi_utils.rs /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/tz_linux.rs
-
-/home/one/dsfb/crates/dsfb-srd/target/debug/deps/libiana_time_zone-3c936cc947597cb1.rlib: /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/lib.rs /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/ffi_utils.rs /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/tz_linux.rs
-
-/home/one/dsfb/crates/dsfb-srd/target/debug/deps/libiana_time_zone-3c936cc947597cb1.rmeta: /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/lib.rs /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/ffi_utils.rs /home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/tz_linux.rs
-
-/home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/lib.rs:
-/home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/ffi_utils.rs:
-/home/one/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.65/src/tz_linux.rs:
